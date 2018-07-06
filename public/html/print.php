9f649f66979ec14f382bd98772a53bbcd4091096
ysiouda <ysiouda@erasme.org>
2018-07-06 15:58:53 +0200

    FEAT Added print.php

---
+++ public/html/print.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+        <link rel="stylesheet" type="text/css" href="../css/style.css" />
+        <link rel="stylesheet" type="text/css" href="../css/print.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-1">
+                <!-- INTRODUCTION -->
+                <div class="introduction">
+                    <div class="logo">
+                        <img width="100%" src="../resources/pictures/logo_erasme.png">
+                    </div>
+                    <p>Lorem ipsum sit dolor sit amet cosecudur testetsgqisfsdf</p>
+                </div>
+                
+            </div>
+            <!--MAP DISPLAY-->
+            <div class="col-2">
+                    <?php include("../resources/pictures/grand_lyon.svg"); ?> 
+            </div>
+            <div class="col-3">
+            </div>
+        </div>
+        <!-- JQUERY -->
+        <script src="../js/jquery-3.3.1.min.js"></script>
+        <!-- CUSTOM -->
+        <script src="../js/script.js"></script>
+    </body>
+</html>
