9c8e486df3d117ee886363b8608f29e9c92475aa
ysiouda <ysiouda@erasme.org>
2018-10-30 11:55:49 +0100

    FEAT Added Font Awesome

--- public/html/index.php
+++ public/html/index.php
@@ -2,8 +2,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-        <link rel="stylesheet" type="text/css" href="../css/style.css" />
-        <link rel="stylesheet" type="text/css" href="../css/print.css" />
+        <link rel="stylesheet" href="../fonts/fontawesome-5.4.1/css/all.css">
     </head>
     <body>
         <div class="container">
